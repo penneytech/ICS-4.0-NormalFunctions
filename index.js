373cfafe2a6dcd00ad004673bcedf01e1ba6fe0b
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-10 09:32:43 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+In this excercise you will make a simple function that increases a counter. 
+
+1) Declare a variable called "counter", and set it's initial value to zero.
+2) Create a function called "countUp".
+3) Inside the "countUp" function, increment the 'counter' variable, and then log it's value to the console.
+4) Call the function. 
+*/
