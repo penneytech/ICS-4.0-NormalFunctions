20052f3d442b3cf11692b73b0fcec100956dd7df
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-11 11:05:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /*
-In this excercise you will make a simple function that increases a counter. 
+
+In this excercise you will make a simple function that increases the numerical value of a variable, 'counter'. 
 
 1) Declare a variable called "counter", and set it's initial value to zero.
 2) Create a function called "countUp".
-3) Inside the "countUp" function, increment the 'counter' variable, and then log it's value to the console.
-4) Call the function. 
+3) Inside the "countUp" function, increment the 'counter' variable, and then log it's value to the console. If unsure how, google "How to increment number javascript".
+4) Call the function 5 times to make the count increase by 5.
+
 */
